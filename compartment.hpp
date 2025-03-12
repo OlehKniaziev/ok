c3eab07f1b51ce2d257d54a1eeadaf4a6e69f7d1
atmatm9182 <atmatm9182@gmail.com>
2025-03-12 10:01:18 +0100

    Fixed broken code and added the `COMPARTMENT_IMPLEMENTATION` guard

--- compartment.hpp
+++ compartment.hpp
@@ -1,10 +1,11 @@
 #ifndef COMPARTMENT_H_
 #define COMPARTMENT_H_
 
-#ifndef COMT_NO_STD
-
 #include <cstdlib>
 #include <cstdio>
+#include <cstring>
+#include <cstdint>
+#include <cstdarg>
 
 #ifndef COMT_ASSERT
 #define COMT_ASSERT(x) do { \
@@ -27,91 +28,72 @@ exit(1); \
     __builtin_trap(); \
     } while (0)
 
-#endif
-
 namespace comt {
-#define LIST_GROW_FACTOR(x) ((((x) + 1) * 3) >> 1)
+struct Allocator {
+    // required methods
+    virtual void* raw_alloc(size_t size) = 0;
+    virtual void raw_dealloc(void* ptr, size_t size) = 0;
+
+    // optional methods
+    virtual void* raw_resize(void* ptr, size_t old_size, size_t new_size) {
+        void* new_ptr = raw_alloc(new_size);
+        memcpy(new_ptr, ptr, old_size);
+        raw_dealloc(ptr, old_size);
+        return new_ptr;
+    }
+
+    // provided methods
+    template <typename T>
+    inline T* alloc(size_t size) {
+        return (T*)raw_alloc(sizeof(T) * size);
+    }
+
+    template <typename T>
+    inline void dealloc(T* ptr, size_t count) {
+        return raw_dealloc((void*)ptr, count * sizeof(T));
+    }
+
+    template <typename T>
+    inline T* resize(T* ptr, size_t old_size, size_t new_size) {
+        T* new_ptr = alloc<T>(new_size);
+        memcpy((void*)new_ptr, ptr, old_size * sizeof(T));
+        dealloc<T>(ptr, old_size);
+        return new_ptr;
+    }
+};
+
+struct ArenaAllocator : public Allocator {
+    void* raw_alloc(size_t size) override {
+        COMT_UNUSED(size);
+        COMT_TODO();
+    }
+
+    void raw_dealloc(void* ptr, size_t size) override {
+        COMT_UNUSED(ptr);
+        COMT_UNUSED(size);
+        COMT_TODO();
+    }
+};
+
+#define COMT_LIST_GROW_FACTOR(x) ((((x) + 1) * 3) >> 1)
 
 template <typename T>
 struct List {
-    static List<T> alloc(Arena* a, size_t cap = List::DEFAULT_CAP) {
-        List<T> list{};
-        list.items = (T*)arena_alloc(a, sizeof(T) * cap);
-        list.count = 0;
-        list.capacity = cap;
-        list.arena = a;
-
-        return list;
-    }
+    static List<T> alloc(Allocator* a, size_t cap = List::DEFAULT_CAP);
 
     static constexpr size_t DEFAULT_CAP = 7;
 
-    inline void push(const T& item) {
-        if (count >= capacity) {
-            items = (T*)arena_realloc(arena, items, sizeof(T) * capacity, sizeof(T) * LIST_GROW_FACTOR(capacity));
-            capacity = LIST_GROW_FACTOR(capacity);
-        }
+    void push(const T& item);
+    void extend(List<T> other);
+    void remove_at(size_t idx);
 
-        items[count++] = item;
-    }
+    List<T> copy(size_t start, size_t end) const;
 
-    inline void remove_at(size_t idx) {
-        COMT_ASSERT(idx < count);
+    void reserve(size_t new_cap);
 
-        for (size_t i = idx; i < count - 1; i++) {
-            items[i] = items[i + 1];
-        }
-
-        count--;
-    }
-
-    inline List<T> copy(size_t start, size_t end) const {
-        COMT_ASSERT(end >= start);
-
-        auto res = List<T>::alloc(arena, end - start);
-        for (size_t i = start; i < end; i++) {
-            res.push(items[i]);
-        }
-        return res;
-    }
-
-    inline void extend(List<T> other) {
-        reserve(capacity + other.capacity);
-
-        for (size_t i = 0; i < other.count; i++) {
-            push(other.items[i]);
-        }
-    }
-
-    inline void reserve(size_t new_cap) {
-        if (new_cap <= capacity) {
-            return;
-        }
-
-        items = (T*)arena_realloc(arena, items, sizeof(T) * capacity, sizeof(T) * new_cap);
-        capacity = new_cap;
-    }
-
-    inline size_t find_index(const T& elem) {
-        for (size_t i = 0; i < count; i++) {
-            if (items[i] == elem) {
-                return i;
-            }
-        }
-
-        return (size_t)-1;
-    }
-
+    size_t find_index(const T& elem);
     template <typename F>
-    inline size_t find_index(F pred) {
-        for (size_t i = 0; i < count; i++) {
-            if (pred(items[i])) {
-                return i;
-            }
-        }
-
-        return (size_t)-1;
-    }
+    size_t find_index(F pred);
 
     inline T& operator [](size_t idx) {
         COMT_ASSERT(idx < count);
@@ -128,14 +110,14 @@ struct List {
     T* items;
     size_t count;
     size_t capacity;
-    Arena* arena;
+    Allocator* allocator;
 };
 
-#define TAB_META_OCCUPIED 0x01
-#define TAB_IS_OCCUPIED(meta) ((meta) & TAB_META_OCCUPIED)
-#define TAB_IS_FREE(meta) (!TAB_IS_OCCUPIED((meta)))
+#define COMT_TAB_META_OCCUPIED 0x01
+#define COMT_TAB_IS_OCCUPIED(meta) ((meta) & COMT_TAB_META_OCCUPIED)
+#define COMT_TAB_IS_FREE(meta) (!COMT_TAB_IS_OCCUPIED((meta)))
 
-#define TABLE_GROWTH_FACTOR(x) (((x) + 1) * 3)
+#define COMT_TABLE_GROWTH_FACTOR(x) (((x) + 1) * 3)
 
 template <typename T>
 struct Hash {};
@@ -147,22 +129,13 @@ struct HashPtr {
     const T* value;
 };
 
+// Default hash implementations
 template <typename T>
 struct Hash<HashPtr<T>> {
     static uint64_t hash(const HashPtr<T>& ptr) {
         return Hash<T>::hash(*ptr.value);
     }
 };
-
-template <typename T>
-bool operator ==(const HashPtr<T>& lhs, const HashPtr<T>& rhs) {
-    return *lhs.value == *rhs.value;
-}
-
-template <typename T>
-std::ostream& operator <<(std::ostream& out, const HashPtr<T>& rhs) {
-    return out << *rhs.value;
-}
 
 template <>
 struct Hash<uint32_t> {
@@ -178,9 +151,12 @@ struct Hash<size_t> {
     }
 };
 
+template <typename T>
+bool operator ==(const HashPtr<T>& lhs, const HashPtr<T>& rhs);
+
 #define TABLE_FOREACH(tab, key, value, code) do { \
     for (size_t _tab_i = 0; _tab_i < (tab).capacity; _tab_i++) {\
-    if (TAB_IS_FREE((tab).meta[_tab_i])) continue; \
+    if (COMT_TAB_IS_FREE((tab).meta[_tab_i])) continue; \
     auto key = (tab).keys[_tab_i]; \
     auto value = (tab).values[_tab_i]; \
     code; \
@@ -189,87 +165,17 @@ struct Hash<size_t> {
 
 template <typename K, typename V>
 struct Table {
-    static Table<K, V> alloc(Arena* a, size_t capacity = Table::DEFAULT_CAPACITY) {
-        Table<K, V> tab{};
+    using Meta = uint8_t;
 
-        tab.keys = (K*)arena_alloc(a, sizeof(K) * capacity);
-        tab.values = (V*)arena_alloc(a, sizeof(V) * capacity);
-        tab.meta = (uint8_t*)arena_alloc(a, sizeof(uint8_t) * capacity);
-        tab.count = 0;
-        tab.capacity = capacity;
-        tab.arena = a;
+    static Table<K, V> alloc(Allocator* a, size_t capacity = Table::DEFAULT_CAPACITY);
 
-        return tab;
-    }
+    void put(const K& key, const V& value);
+    bool get(const K& key, V* out);
+    bool has(const K& key);
 
     static constexpr size_t DEFAULT_CAPACITY = 47;
 
-    void put(const K& key, const V& value) {
-        if (load_percentage() >= 70) {
-            auto new_table = Table<K, V>::alloc(arena, TABLE_GROWTH_FACTOR(capacity));
-
-            for (size_t i = 0; i < capacity; i++) {
-                if (TAB_IS_OCCUPIED(meta[i])) {
-                    new_table.put(keys[i], values[i]);
-                }
-            }
-
-            *this = new_table;
-        }
-
-        uint64_t idx = Hash<K>::hash(key) % capacity;
-
-        while (true) {
-            if (TAB_IS_FREE(meta[idx])) {
-                meta[idx] |= TAB_META_OCCUPIED;
-                values[idx] = value;
-                keys[idx] = key;
-                count++;
-                return;
-            }
-
-            if (keys[idx] == key) {
-                values[idx] = value;
-                keys[idx] = key;
-                return;
-            }
-
-            idx = (idx + 1) % capacity;
-        }
-    }
-
-    bool get(const K& key, V* out) {
-        uint64_t idx = Hash<K>::hash(key) % capacity;
-        uint64_t initial_idx = idx;
-
-        do {
-            if (TAB_IS_OCCUPIED(meta[idx]) && keys[idx] == key) {
-                *out = values[idx];
-                return true;
-            }
-
-            idx = (idx + 1) % capacity;
-        } while (idx != initial_idx);
-
-        return false;
-    }
-
-    bool has(const K& key) {
-        uint64_t idx = Hash<K>::hash(key) % capacity;
-        uint64_t initial_idx = idx;
-
-        do {
-            if (TAB_IS_OCCUPIED(meta[idx]) && keys[idx] == key) {
-                return true;
-            }
-
-            idx = (idx + 1) % capacity;
-        } while (idx != initial_idx);
-
-        return false;
-    }
-
-    uint8_t load_percentage() const {
+    inline uint8_t load_percentage() const {
         return (uint8_t)((double)(count * 100) / (double)capacity);
     }
 
@@ -278,7 +184,7 @@ struct Table {
     uint8_t* meta;
     size_t count;
     size_t capacity;
-    Arena* arena;
+    Allocator* allocator;
 };
 
 struct String {
@@ -287,54 +193,11 @@ struct String {
     static constexpr Char NULL_CHAR = '\0';
     static constexpr size_t DEFAULT_CAPACITY = 7;
 
-    static String alloc(Arena* a, size_t capacity = DEFAULT_CAPACITY) {
-        String s;
-        s.data = List<Char>::alloc(a, capacity + 1);
-        s.data.push(NULL_CHAR);
+    static String alloc(Allocator* a, size_t capacity = DEFAULT_CAPACITY);
+    static String alloc(Allocator* a, const char* data, size_t data_len);
+    static String alloc(Allocator* a, const char* data);
 
-        return s;
-    }
-
-    static String alloc(Arena* a, const char* data, size_t data_len) {
-        auto s = String::alloc(a, data_len);
-
-        for (size_t i = 0; i < data_len; i++) s.push((Char)data[i]);
-
-        return s;
-    }
-
-    static String alloc(Arena* a, const char* data) {
-        size_t data_len = strlen(data);
-        return String::alloc(a, data, data_len);
-    }
-
-    static String format(Arena* a, const char* fmt, ...) {
-        va_list sprintf_args;
-
-        int buf_size;
-        va_start(sprintf_args, a);
-        {
-            char* sprintf_buf = nullptr;
-            buf_size = vsprintf(sprintf_buf, fmt, sprintf_args);
-            COMT_ASSERT(buf_size != -1);
-
-            buf_size += 1;
-        }
-        va_end(sprintf_args);
-
-        auto buf = String::alloc(a, buf_size);
-
-        va_start(sprintf_args, a);
-        {
-            int bytes_written = vsprintf((char*)buf.data.items, fmt, sprintf_args);
-            COMT_ASSERT(bytes_written != -1);
-        }
-        va_end(sprintf_args);
-
-        buf.data.count = buf_size;
-
-        return buf;
-    }
+    static String format(Allocator* a, const char* fmt, ...);
 
     inline void push(Char character) {
         data.push(NULL_CHAR);
@@ -349,79 +212,30 @@ struct String {
     List<Char> data;
 };
 
+#define COMT_SET_GROWTH_FACTOR COMT_TABLE_GROWTH_FACTOR
+
 template <typename T>
 struct Set {
-    static Set<T> alloc(Arena* a, size_t capacity = DEFAULT_CAPACITY) {
-        Set<T> set;
-        set.arena = a;
-        set.count = 0;
-        set.capacity = capacity;
-        set.values = (T*)arena_alloc(a, sizeof(T) * capacity);
-        set.meta = (uint8_t*)arena_alloc(a, sizeof(uint8_t) * capacity);
-        return set;
-    }
+    using Meta = uint8_t;
 
     static constexpr size_t DEFAULT_CAPACITY = 47;
 
-    inline void put(const T& elem) {
-        if (load_percentage() >= 70) {
-            auto new_set = Set<T>::alloc(arena, TABLE_GROWTH_FACTOR(capacity));
+    static Set<T> alloc(Allocator* a, size_t capacity = DEFAULT_CAPACITY);
 
-            for (size_t i = 0; i < capacity; i++) {
-                if (TAB_IS_FREE(meta[i])) {
-                    continue;
-                }
+    void put(const T& elem);
+    bool has(const T& elem) const;
 
-                new_set.put(values[i]);
-            }
-
-            *this = new_set;
-        }
-
-        uint64_t hash = Hash<T>::hash(elem);
-
-        while (true) {
-            if (TAB_IS_FREE(meta[hash])) {
-                meta[hash] |= TAB_META_OCCUPIED;
-                values[hash] = elem;
-                count++;
-                return;
-            }
-
-            if (values[hash] == elem) {
-                values[hash] = elem;
-                return;
-            }
-
-            hash = (hash + 1) % capacity;
-        }
-    }
-
-    bool has(const T& elem) const {
-        uint64_t hash = Hash<T>::hash(elem);
-        uint64_t idx = hash;
-
-        do {
-            if (TAB_IS_OCCUPIED(meta[idx]) && values[idx] == elem) {
-                return true;
-            }
-
-            idx = (idx + 1) % capacity;
-        } while (idx != hash);
-
-        return false;
-    }
-
-    uint8_t load_percentage() const {
+    inline uint8_t load_percentage() const {
         return (uint8_t)(100.0 * (double)count / (double)capacity);
     }
 
-    Arena* arena;
+    Allocator* allocator;
     size_t capacity;
     size_t count;
     T* values;
     uint8_t* meta;
 };
+
 template <typename T>
 struct Optional {
     Optional() : _has_value{false} {}
@@ -463,11 +277,306 @@ struct Optional<T*> {
     static const Optional<T> NONE;
 };
 
+#ifdef COMPARTMENT_IMPLEMENTATION
+
+// HASH IMPLEMENTATION
+template <typename T>
+bool operator ==(const HashPtr<T>& lhs, const HashPtr<T>& rhs) {
+    return *lhs.value == *rhs.value;
+}
+
+// OPTIONAL IMPLEMENTATION
 template <typename T>
 const Optional<T> Optional<T>::NONE = Optional<T>{};
 
 template <typename T>
 const Optional<T> Optional<T*>::NONE = Optional<T*>{};
+
+// LIST IMPLEMENTATION
+template <typename T>
+List<T> List<T>::alloc(Allocator* a, size_t cap) {
+    List<T> list{};
+    list.items = a->alloc<T>(cap);
+    list.count = 0;
+    list.capacity = cap;
+    list.allocator = a;
+
+    return list;
+}
+
+template <typename T>
+void List<T>::push(const T& item) {
+    if (count >= capacity) {
+        size_t new_capacity = COMT_LIST_GROW_FACTOR(capacity);
+        items = allocator->resize<T>(items, capacity, new_capacity);
+        capacity = new_capacity;
+    }
+
+    items[count++] = item;
+}
+
+template <typename T>
+inline void List<T>::remove_at(size_t idx) {
+    COMT_ASSERT(idx < count);
+
+    for (size_t i = idx; i < count - 1; i++) {
+        items[i] = items[i + 1];
+    }
+
+    count--;
+}
+
+template <typename T>
+inline List<T> List<T>::copy(size_t start, size_t end) const {
+    COMT_ASSERT(end >= start);
+
+    auto res = List<T>::alloc(allocator, end - start);
+    for (size_t i = start; i < end; i++) {
+        res.push(items[i]);
+    }
+    return res;
+}
+
+template <typename T>
+inline void List<T>::extend(List<T> other) {
+    reserve(capacity + other.capacity);
+
+    for (size_t i = 0; i < other.count; i++) {
+        push(other.items[i]);
+    }
+}
+
+template <typename T>
+inline void List<T>::reserve(size_t new_cap) {
+    if (new_cap <= capacity) {
+        return;
+    }
+
+    items = allocator->resize<T>(items, capacity, new_cap);
+    capacity = new_cap;
+}
+
+template <typename T>
+inline size_t List<T>::find_index(const T& elem) {
+    for (size_t i = 0; i < count; i++) {
+        if (items[i] == elem) {
+            return i;
+        }
+    }
+
+    return (size_t)-1;
+}
+
+template <typename T>
+template <typename F>
+inline size_t List<T>::find_index(F pred) {
+    for (size_t i = 0; i < count; i++) {
+        if (pred(items[i])) {
+            return i;
+        }
+    }
+
+    return (size_t)-1;
+}
+
+// TABLE IMPLEMENTATION
+template <typename K, typename V>
+Table<K, V> Table<K, V>::alloc(Allocator* a, size_t capacity) {
+    Table<K, V> tab{};
+
+    tab.keys = a->alloc<K>(capacity);
+    tab.values = a->alloc<V>(capacity);
+    tab.meta = a->alloc<Meta>(capacity);
+    tab.count = 0;
+    tab.capacity = capacity;
+    tab.allocator = a;
+
+    return tab;
+}
+
+template <typename K, typename V>
+void Table<K, V>::put(const K& key, const V& value) {
+    if (load_percentage() >= 70) {
+        auto new_table = Table<K, V>::alloc(allocator, COMT_TABLE_GROWTH_FACTOR(capacity));
+
+        for (size_t i = 0; i < capacity; i++) {
+            if (COMT_TAB_IS_OCCUPIED(meta[i])) {
+                new_table.put(keys[i], values[i]);
+            }
+        }
+
+        *this = new_table;
+    }
+
+    uint64_t idx = Hash<K>::hash(key) % capacity;
+
+    while (true) {
+        if (COMT_TAB_IS_FREE(meta[idx])) {
+            meta[idx] |= COMT_TAB_META_OCCUPIED;
+            values[idx] = value;
+            keys[idx] = key;
+            count++;
+            return;
+        }
+
+        if (keys[idx] == key) {
+            values[idx] = value;
+            keys[idx] = key;
+            return;
+        }
+
+        idx = (idx + 1) % capacity;
+    }
+}
+
+template <typename K, typename V>
+bool Table<K, V>::get(const K& key, V* out) {
+    uint64_t idx = Hash<K>::hash(key) % capacity;
+    uint64_t initial_idx = idx;
+
+    do {
+        if (COMT_TAB_IS_OCCUPIED(meta[idx]) && keys[idx] == key) {
+            *out = values[idx];
+            return true;
+        }
+
+        idx = (idx + 1) % capacity;
+    } while (idx != initial_idx);
+
+    return false;
+}
+
+template <typename K, typename V>
+bool Table<K, V>::has(const K& key) {
+    uint64_t idx = Hash<K>::hash(key) % capacity;
+    uint64_t initial_idx = idx;
+
+    do {
+        if (COMT_TAB_IS_OCCUPIED(meta[idx]) && keys[idx] == key) {
+            return true;
+        }
+
+        idx = (idx + 1) % capacity;
+    } while (idx != initial_idx);
+
+    return false;
+}
+
+// STRING IMPLEMENTATION
+
+String String::alloc(Allocator* a, size_t capacity) {
+    String s;
+    s.data = List<Char>::alloc(a, capacity + 1);
+    s.data.push(NULL_CHAR);
+
+    return s;
+}
+
+String String::alloc(Allocator* a, const char* data, size_t data_len) {
+    auto s = String::alloc(a, data_len);
+
+    for (size_t i = 0; i < data_len; i++) s.push((Char)data[i]);
+
+    return s;
+}
+
+String String::alloc(Allocator* a, const char* data) {
+    size_t data_len = strlen(data);
+    return String::alloc(a, data, data_len);
+}
+
+String String::format(Allocator* a, const char* fmt, ...) {
+    va_list sprintf_args;
+
+    int buf_size;
+    va_start(sprintf_args, fmt);
+    {
+        char* sprintf_buf = nullptr;
+        buf_size = vsprintf(sprintf_buf, fmt, sprintf_args);
+        COMT_ASSERT(buf_size != -1);
+
+        buf_size += 1;
+    }
+    va_end(sprintf_args);
+
+    auto buf = String::alloc(a, buf_size);
+
+    va_start(sprintf_args, fmt);
+    {
+        int bytes_written = vsprintf((char*)buf.data.items, fmt, sprintf_args);
+        COMT_ASSERT(bytes_written != -1);
+    }
+    va_end(sprintf_args);
+
+    buf.data.count = buf_size;
+
+    return buf;
+}
+
+// SET IMPLEMENTATION
+template <typename T>
+Set<T> Set<T>::alloc(Allocator* a, size_t capacity) {
+    Set<T> set;
+    set.allocator = a;
+    set.count = 0;
+    set.capacity = capacity;
+    set.values = a->alloc<T>(capacity);
+    set.meta = a->alloc<Meta>(capacity);
+    return set;
+}
+
+template <typename T>
+void Set<T>::put(const T& elem) {
+    if (load_percentage() >= 70) {
+        auto new_set = Set<T>::alloc(allocator, COMT_SET_GROWTH_FACTOR(capacity));
+
+        for (size_t i = 0; i < capacity; i++) {
+            if (COMT_TAB_IS_FREE(meta[i])) {
+                continue;
+            }
+
+            new_set.put(values[i]);
+        }
+
+        *this = new_set;
+    }
+
+    uint64_t hash = Hash<T>::hash(elem);
+
+    while (true) {
+        if (COMT_TAB_IS_FREE(meta[hash])) {
+            meta[hash] |= COMT_TAB_META_OCCUPIED;
+            values[hash] = elem;
+            count++;
+            return;
+        }
+
+        if (values[hash] == elem) {
+            values[hash] = elem;
+            return;
+        }
+
+        hash = (hash + 1) % capacity;
+    }
+}
+
+template <typename T>
+bool Set<T>::has(const T& elem) const {
+    uint64_t hash = Hash<T>::hash(elem);
+    uint64_t idx = hash;
+
+    do {
+        if (COMT_TAB_IS_OCCUPIED(meta[idx]) && values[idx] == elem) {
+            return true;
+        }
+
+        idx = (idx + 1) % capacity;
+    } while (idx != hash);
+
+    return false;
+}
+
+#endif
 };
 
 #endif // COMPARTMENT_H_
