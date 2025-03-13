ccccc510f9abcd41ee13205708983f483bc31fb0
atmatm9182 <atmatm9182@gmail.com>
2025-03-13 19:15:33 +0100

    Improved the string test case

--- tests/string.cpp
+++ tests/string.cpp
@@ -4,11 +4,27 @@
 using namespace comt;
 
 int main() {
-    ArenaAllocator a{};
+    Allocator* a = temp_allocator;
 
-    const char* str = "hello";
+    const char* hello = "hello";
 
-    String s = String::alloc(&a, str);
+    String s = String::alloc(a, hello);
 
-    COMT_ASSERT(strcmp(s.cstr(), str) == 0);
+    COMT_ASSERT(strcmp(s.cstr(), hello) == 0);
+
+#define hello_world "hello world!"
+
+    String message = String::format(a, "%s world!", hello);
+
+    COMT_ASSERT(strcmp(message.cstr(), hello_world) == 0);
+
+    message.append(" and friends"_sv);
+
+    COMT_ASSERT(strcmp(message.cstr(), hello_world " and friends") == 0);
+
+    const char* obviously_the_best_language = "C++";
+
+    message.format_append(" from %s", obviously_the_best_language);
+
+    COMT_ASSERT(strcmp(message.cstr(), hello_world " and friends from C++") == 0);
 }
