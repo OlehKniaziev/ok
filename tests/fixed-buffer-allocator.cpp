50bca478ea7607dd6ba919109aefe09994f55b2d
atmatm9182 <atmatm9182@gmail.com>
2025-03-13 12:37:51 +0100

    Modified the `FixedBufferAllocator` test case to reflect recent changes

--- tests/fixed-buffer-allocator.cpp
+++ tests/fixed-buffer-allocator.cpp
@@ -4,16 +4,22 @@
 using namespace comt;
 
 int main() {
+    const char* hello = "hello";
+    const char* not_hello = "not hello";
+
     FixedBufferAllocator a{};
 
-    String s = String::alloc(&a, "hello");
+    String hello_string = String::alloc(&a, hello);
 
-    COMT_ASSERT(strcmp((const char*)s.data.items, "hello") == 0);
+    COMT_ASSERT(strcmp(hello_string.cstr(), hello) == 0);
 
-    String s1 = String::alloc(&a, "not hello");
+    String not_hello_string = String::alloc(&a, not_hello);
 
-    COMT_ASSERT(strcmp(s.cstr(), "hello") == 0);
-    COMT_ASSERT(strcmp(s1.cstr(), "not hello") == 0);
+    COMT_ASSERT(strcmp(hello_string.cstr(), hello) == 0);
+    COMT_ASSERT(strcmp(not_hello_string.cstr(), not_hello) == 0);
+
+    String* very_big_allocation = a.alloc<String>(1'000'000);
+    COMT_ASSERT(very_big_allocation == nullptr);
 
     return 0;
 }
